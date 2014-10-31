71a59c4e4f6dbf73e20eba689af052c0c56d97a2
ys2n <ys2n@virginia.edu>
2014-10-31 15:42:34 -0400

    went with 2.4.1 instead of main/HEAD from fancytree's github (which was broken)

--- libraries/fancytree/jquery.fancytree.filter.js
+++ libraries/fancytree/jquery.fancytree.filter.js
@@ -1,72 +1,43 @@
-/*************************************************************************
-	jquery.fancytree.filter.js
-	Table extension for jquery.fancytree.js.
+/*!
+ * jquery.fancytree.filter.js
+ *
+ * Remove or highlight tree nodes, based on a filter.
+ * (Extension module for jquery.fancytree.js: https://github.com/mar10/fancytree/)
+ *
+ * Copyright (c) 2014, Martin Wendt (http://wwWendt.de)
+ *
+ * Released under the MIT license
+ * https://github.com/mar10/fancytree/wiki/LicenseInfo
+ *
+ * @version @VERSION
+ * @date @DATE
+ */
 
-	Copyright (c) 2012, Martin Wendt (http://wwWendt.de)
-	Dual licensed under the MIT or GPL Version 2 licenses.
-	http://code.google.com/p/fancytree/wiki/LicenseInfo
-
-	A current version and some documentation is available at
-		http://fancytree.googlecode.com/
-*************************************************************************/
-
-// Start of local namespace
-(function($) {
-// relax some jslint checks:
-/*globals alert */
+;(function($, window, document, undefined) {
 
 "use strict";
-
-// prevent duplicate loading
-// if ( $.ui.fancytree && $.ui.fancytree.version ) {
-//     $.ui.fancytree.warn("Fancytree: duplicate include");
-//     return;
-// }
 
 
 /*******************************************************************************
  * Private functions and variables
  */
-function _raiseNotImplemented(msg){
-	msg = msg || "";
-	$.error("Not implemented: " + msg);
-}
-
-
-function _assert(cond, msg){
-	msg = msg || "";
-	if(!cond){
-		$.error("Assertion failed " + msg);
-	}
-}
-
 
 function _escapeRegex(str){
 	/*jshint regexdash:true */
 	return (str + "").replace(/([.?*+\^\$\[\]\\(){}|-])/g, "\\$1");
 }
 
-
-/**
- * Dimm or hide nodes.
- *
- * @param {function | string} filter
- * @returns {integer} count
- * @lends Fancytree.prototype
- * @requires jquery.fancytree.filter.js
- */
-$.ui.fancytree._FancytreeClass.prototype.applyFilter = function(filter){
-	var count = 0;
-	// Reset current filter
-	this.visit(function(node){
-		delete node.match;
-		delete node.subMatch;
-	});
+$.ui.fancytree._FancytreeClass.prototype._applyFilterImpl = function(filter, branchMode, leavesOnly){
+	var match, re,
+		count = 0,
+		hideMode = this.options.filter.mode === "hide";
+		// leavesOnly = !branchMode && this.options.filter.leavesOnly;
+	leavesOnly = !!leavesOnly && !branchMode;
 
 	// Default to 'match title substring (not case sensitive)'
 	if(typeof filter === "string"){
-		var match = _escapeRegex(filter), // make sure a '.' is treated literally
-			re = new RegExp(".*" + match + ".*", "i");
+		match = _escapeRegex(filter); // make sure a '.' is treated literally
+		re = new RegExp(".*" + match + ".*", "i");
 		filter = function(node){
 			return !!re.exec(node.title);
 		};
@@ -74,86 +45,117 @@ $.ui.fancytree._FancytreeClass.prototype.applyFilter = function(filter){
 
 	this.enableFilter = true;
 	this.$div.addClass("fancytree-ext-filter");
+	if( hideMode ){
+		this.$div.addClass("fancytree-ext-filter-hide");
+	} else {
+		this.$div.addClass("fancytree-ext-filter-dimm");
+	}
+	// Reset current filter
 	this.visit(function(node){
-		if(filter(node)){
+		delete node.match;
+		delete node.subMatch;
+	});
+	// Adjust node.hide, .match, .subMatch flags
+	this.visit(function(node){
+		if ((!leavesOnly || node.children == null) && filter(node)) {
 			count++;
 			node.match = true;
 			node.visitParents(function(p){
 				p.subMatch = true;
 			});
+			if( branchMode ) {
+				node.visit(function(p){
+					p.match = true;
+				});
+				return "skip";
+			}
 		}
 	});
+	// Redraw
 	this.render();
 	return count;
 };
 
 /**
- * Reset the filter.
+ * [ext-filter] Dimm or hide nodes.
  *
- * @lends Fancytree.prototype
+ * @param {function | string} filter
+ * @param {boolean} [leavesOnly=false]
+ * @returns {integer} count
+ * @alias Fancytree#filterNodes
+ * @requires jquery.fancytree.filter.js
+ */
+$.ui.fancytree._FancytreeClass.prototype.filterNodes = function(filter, leavesOnly){
+	return this._applyFilterImpl(filter, false, leavesOnly);
+};
+
+$.ui.fancytree._FancytreeClass.prototype.applyFilter = function(filter){
+	this.warn("Fancytree.applyFilter() is deprecated since 2014-05-10. Use .filterNodes() instead.");
+	return this.filterNodes.apply(this, arguments);
+};
+
+/**
+ * [ext-filter] Dimm or hide whole branches.
+ *
+ * @param {function | string} filter
+ * @returns {integer} count
+ * @alias Fancytree#filterBranches
+ * @requires jquery.fancytree.filter.js
+ */
+$.ui.fancytree._FancytreeClass.prototype.filterBranches = function(filter){
+	return this._applyFilterImpl(filter, true, null);
+};
+
+
+/**
+ * [ext-filter] Reset the filter.
+ *
+ * @alias Fancytree#clearFilter
  * @requires jquery.fancytree.filter.js
  */
 $.ui.fancytree._FancytreeClass.prototype.clearFilter = function(){
 	this.visit(function(node){
 		delete node.match;
 		delete node.subMatch;
-		$(node.li).show();
 	});
-
 	this.enableFilter = false;
+	this.$div.removeClass("fancytree-ext-filter fancytree-ext-filter-dimm fancytree-ext-filter-hide");
 	this.render();
-	this.$div.removeClass("fancytree-ext-filter");
 };
 
 
 /*******************************************************************************
  * Extension code
  */
-$.ui.fancytree.registerExtension("filter", {
+$.ui.fancytree.registerExtension({
+	name: "filter",
+	version: "0.2.0",
 	// Default options for this extension.
 	options: {
 		mode: "dimm"
+//		leavesOnly: false
 	},
-	// Override virtual methods for this extension.
-	// `this`       : is this extension object
-	// `this._base` : the Fancytree instance
-	// `this._super`: the virtual function that was overriden (member of prev. extension or Fancytree)
 	treeInit: function(ctx){
-		this._super(ctx);
-		ctx.tree.filter = false;
-	},
-	treeDestroy: function(ctx){
 		this._super(ctx);
 	},
 	nodeRenderStatus: function(ctx) {
 		// Set classes for current status
-		var node = ctx.node,
-			opts = ctx.options,
+		var res,
+			node = ctx.node,
 			tree = ctx.tree,
 			$span = $(node[tree.statusClassPropName]);
 
-		if(!$span.length){
-			return; // nothing to do, if node was not yet rendered
+		res = this._super(ctx);
+		// nothing to do, if node was not yet rendered
+		if( !$span.length || !tree.enableFilter ) {
+			return res;
 		}
-		this._super(ctx);
-		if(!tree.enableFilter){
-			return;
-		}
-		if( node.match ){
-			$span.addClass("fancytree-match");
-		}else{
-			$span.removeClass("fancytree-match");
-		}
-		if( node.subMatch ){
-			$span.addClass("fancytree-submatch");
-		}else{
-			$span.removeClass("fancytree-submatch");
-		}
-		if(opts.filter.mode === "hide"){
-			var visible = !!(node.match || node.subMatch);
-			node.debug(node.title + ": visible=" + visible);
-			$(node.li).toggle(visible);
-		}
+		$span
+			.toggleClass("fancytree-match", !!node.match)
+			.toggleClass("fancytree-submatch", !!node.subMatch)
+			.toggleClass("fancytree-hide", !(node.match || node.subMatch));
+
+		return res;
 	}
 });
-}(jQuery));
+}(jQuery, window, document));
