8a4e6559cd7884fe5b7594a7665d4810516311d7
ys2n <ys2n@virginia.edu>
2014-11-21 16:08:01 -0500

    Refactored to expose "doSearch()" independent of event handling in the form input so that search can be triggered by outside code.

--- libraries/ajaxsolr/shanti/TextWidget.js
+++ libraries/ajaxsolr/shanti/TextWidget.js
@@ -7,6 +7,77 @@ const SEARCH_MIN_LENGTH = 2;
 (function ($) {
 
     AjaxSolr.TextWidget = AjaxSolr.AbstractTextWidget.extend({
+        doSearch: function (widget) {
+
+            console.log("doSearch!");
+
+            if (!widget) {
+                widget = this;
+            }
+            var value = $(widget.target).val();
+
+            if (value.length < SEARCH_MIN_LENGTH) {
+                if (widget.notify) {
+                    widget.notify.clear();
+                    widget.notify.warn('warntooshort', 'Search string must be ' + SEARCH_MIN_LENGTH + ' characters or longer.');
+                }
+            } else {
+                if (widget.notify) {
+                    widget.notify.clear();
+                }
+                //  If the user inserts wildcards, assume they know what they are doing.
+                if (value.indexOf('*') < 0) {
+
+                    // Process advanced search
+
+                    console.log("SEARCH GROUP:" + $('#searchAnchorGroup'));
+                    console.log("SEARCH ANCHOR: " + $('#searchAnchorGroup input[checked]').val());
+                    console.log("SEARCH SCOPE: " + $('#searchScopeGroup input[checked]').val());
+
+                    var anchor = $('#searchAnchorGroup input[checked]').val();
+                    var scope = $('#searchScopeGroup input[checked]').val();
+
+                    switch (anchor) {
+                        case 'startsWith':
+                            value = value + "*";
+                            break;
+                        case 'contains':
+                            value = "*" + value + "*";
+                            break;
+                        case 'exact':
+                            // fall through
+                            break;
+                        default:
+                            alert("Error: unknown anchor: '" + anchor + "'")
+                    }
+
+                    switch (scope) {
+                        case 'name':
+//                                    console.log("name scope");
+                            widget.manager.store.addByValue('df', 'name');
+                            break;
+                        case 'all':
+//                                    console.log('all scope');
+                            widget.manager.store.addByValue('df', 'text');
+                            break;
+                        default:
+                            alert("Error: unknown scope: '" + scope + "'");
+                    }
+                }
+
+                // alert("search " + value);
+//                        console.log("TEXTWIDGET: search " + value);
+
+
+                widget.set(value);
+                if (value /* && self.set(value) */) {
+//                            console.log("TEXTWIDGET: do Request " + value);
+                    widget.doRequest();
+                }
+            }
+        },
+
+
         init: function () {
             var self = this;
 //            console.log("TEXTWIDGET: " + $(this.target))
@@ -16,70 +87,12 @@ const SEARCH_MIN_LENGTH = 2;
 //                console.log("keyup " + e);
                 // alert("blurt");
                 if (e.which == 13) {
-                    var value = $(this).val();
-
-                    if (value.length < SEARCH_MIN_LENGTH) {
-                        if (self.notify) {
-                            self.notify.clear();
-                            self.notify.warn('warntooshort', 'Search string must be ' + SEARCH_MIN_LENGTH + ' characters or longer.');
-                        }
-                    } else {
-                        if (self.notify) {
-                            self.notify.clear();
-                        }
-                        //  If the user inserts wildcards, assume they know what they are doing.
-                        if (value.indexOf('*') < 0) {
-
-                            // Process advanced search
-
-                            console.log("SEARCH GROUP:" + $('#searchAnchorGroup'));
-                            console.log("SEARCH ANCHOR: " + $('#searchAnchorGroup input[checked]').val());
-                            console.log("SEARCH SCOPE: " + $('#searchScopeGroup input[checked]').val());
-
-                            var anchor = $('#searchAnchorGroup input[checked]').val();
-                            var scope = $('#searchScopeGroup input[checked]').val();
-
-                            switch (anchor) {
-                                case 'startsWith':
-                                    value = value + "*";
-                                    break;
-                                case 'contains':
-                                    value = "*" + value + "*";
-                                    break;
-                                case 'exact':
-                                    // fall through
-                                    break;
-                                default:
-                                    alert("Error: unknown anchor: '" + anchor + "'")
-                            }
-
-                            switch (scope) {
-                                case 'name':
-//                                    console.log("name scope");
-                                    self.manager.store.addByValue('df', 'name');
-                                    break;
-                                case 'all':
-//                                    console.log('all scope');
-                                    self.manager.store.addByValue('df', 'text');
-                                    break;
-                                default:
-                                    alert("Error: unknown scope: '" + scope + "'");
-                            }
-                        }
-
-                        // alert("search " + value);
-
-//                        console.log("TEXTWIDGET: search " + value);
-
-
-                        self.set(value);
-                        if (value /* && self.set(value) */) {
-//                            console.log("TEXTWIDGET: do Request " + value);
-                            self.doRequest();
-                        }
-
-                    }
+                    self.doSearch(self);
                 }
+            });
+
+            $(this.target).on('doSearch', function(e) {
+                self.doSearch(self);
             });
 
             $("button.searchreset").click( function() {
